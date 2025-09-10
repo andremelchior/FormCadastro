b981d91ab04f07f6c277860576a9c18c6ce21dff
André Melchior <andre.melchior@outlook.com>
2025-09-09 22:15:39 -0300

    feat: add restrictions on mandatory fields, min and max characters and implement Person class

--- src/app/formulario/formulario.ts
+++ src/app/formulario/formulario.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, FormGroup, FormControl, ReactiveFormsModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css'
 })
 export class Formulario {
-  nome:string;
-  idade:number = null;
-  cidade:string;
+  //Objeto de formulario
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
+    cidade: new FormControl('', [Validators.required, Validators.minLength(3)])
+  });
 }
---
+++ src/app/model/Pessoa.ts
@@ -0,0 +1,11 @@
+interface IPessoa{
+    nome: string;
+    idade: number;
+    cidade: string;
+}
+
+export class Pessoa implements IPessoa{
+    nome: string;
+    idade: number;
+    cidade: string;
+}
