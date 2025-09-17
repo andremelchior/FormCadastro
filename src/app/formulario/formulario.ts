51c17ace54cd4ea217935f4adc6186ea8bdf6368
André Melchior <andre.melchior@outlook.com>
2025-09-16 22:13:23 -0300

    feat: add age and city field constraints, input reset and data selection

--- src/app/formulario/formulario.ts
+++ src/app/formulario/formulario.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from '../model/Pessoa';
 
 @Component({
   selector: 'app-formulario',
-  imports: [FormsModule, CommonModule, FormGroup, FormControl, ReactiveFormsModule],
+  standalone: true,
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css'
 })
@@ -15,4 +17,38 @@ export class Formulario {
     idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
     cidade: new FormControl('', [Validators.required, Validators.minLength(3)])
   });
+
+  //Visibilidade dos botões
+  btnCadastrar: boolean = true;
+  vetor: Pessoa[];
+
+  cadastrar(){
+    //Inclusão da função no vetor
+    this.vetor.push(this.formulario.value as Pessoa);
+
+    //Limpeza dos inputs
+    this.formulario.reset();
+
+    //Visualização no console
+    console.table(this.vetor);
+
+    
+  }
+  //armazenar indice da pessoa selecionada
+  indice: number = -1;
+
+  selecionar(indice: number){
+    //atribuir o indice da pessoa
+    this.indice = indice;
+
+    //atribuir os dados da pessoa no formulario
+
+    this.formulario.setValue({
+      nome: this.vetor[indice].nome,
+      idade: this.vetor[indice].idade,
+      cidade: this.vetor[indice].cidade
+    });
+
+    this.btnCadastrar = false;
+  }
 }
